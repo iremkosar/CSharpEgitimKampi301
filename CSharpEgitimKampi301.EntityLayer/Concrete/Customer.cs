4133c73ec8eb95c22cf381e7a4557c9154e8e0c2
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2025-08-03 22:29:09 +0300

    migration operations

--- CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +15,7 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public string CustomerDistrict { get; set; } 
         public string CustomerCity { get; set; }
         public List<Order> Orders { get; set; } 
+        public bool CustomerStatus {  get; set; }
+
     }
 }
